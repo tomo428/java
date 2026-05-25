51be72e8e16cf7237af740b4231cf0156fa47b51
tomo428 <big.rye.star0527@gmail.com>
2026-05-25 23:02:31 +0900

    java1日目

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// Hello World!の出力
+		System.out.println("Hello World!");
+	}
+
+}
+public class konnitiha {
+	public static void main(String[] args) {
+		System.out.println("こんにちは！！");
+	}
+}
