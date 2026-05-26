560a3bc21438811c32c16c1b0cc7a744c0ff72aa
tomo428 <big.rye.star0527@gmail.com>
2026-05-26 23:10:53 +0900

    java2日目

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,11 +4,13 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// Hello World!の出力
 		System.out.println("Hello World!");
+		//3時間目
+		System.out.println("こんにちは！！");
+		System.out.println(123);
+		System.out.println("おはよう");
+		System.out.println(0);
+		System.out.println("例外が発生しました");
+		System.out.println("例外が\r\n発生しました");
 	}
 
-}
-public class konnitiha {
-	public static void main(String[] args) {
-		System.out.println("こんにちは！！");
-	}
 }
