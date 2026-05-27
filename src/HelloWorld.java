aafc2cc101b93b06904913ccfc1b2087691195a7
tomo428 <big.rye.star0527@gmail.com>
2026-05-27 23:06:09 +0900

    java3日目

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -11,6 +11,54 @@ public class HelloWorld {
 		System.out.println(0);
 		System.out.println("例外が発生しました");
 		System.out.println("例外が\r\n発生しました");
+//		4時間目
+		String name = "高橋";
+		System.out.println(name);
+
+		String str1 = "鈴木";
+		System.out.println(str1);
+
+		String str2 = "Tanaka";
+		System.out.println(str2);
+
+		String tel = "090-1234-5678";
+		System.out.println(tel);
+
+		char char1 ='A';
+		System.out.println(char1);
+
+		int num1 = 12345;
+		System.out.println(num1);
+
+		boolean boo = true;
+		System.out.println(boo);
+
+		int a = 3+3;
+		System.out.println(a);
+
+		String a2 = "3+3";
+		System.out.println(a2);
+
+		String abc = "山田";
+		System.out.println(abc + "さん");
+
+//		5時間目
+		float b1 = 5 / 2;
+		System.out.println(b1);
+
+		float b2 = (float)5 / 2;
+		System.out.println(b2);
+
+		float b3 = (float)10 / 3;
+		System.out.println(b3);
+
+		double b4 = (double)10 / 3;
+		System.out.println(b4);
+
+		int b5 = 7 % 3;
+		System.out.println(b5);
+		
+		System.out.println(6 % 3);
 	}
 
 }
